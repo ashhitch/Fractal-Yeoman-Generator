c0332b4b69fe308d7632a5bfc213fbe125d471ff
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2017-08-01 12:55:42 +0100

    Code Improvments and Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# An opinionated Yeoman Generator For [Fractal](http://fractal.build/)
+
+Add to Fractal project and run `yo ./generators/app`
+
+Generator maintained by [Ash Hitchcock](https://www.ashleyhitchcock.com)
--- generators/app/index.js
+++ generators/app/index.js
@@ -12,19 +12,16 @@ module.exports = class extends Generator {
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the ' + chalk.red('Fractal') + ' generator!'
+      `Welcome to the ${chalk.red('Fractal')} generator!`
     ));
 
-
-
   }
-
 
   prompting() {
     return this.prompt([{
       type: 'list',
       name: 'type',
-      message: 'What would you like to create?',
+      message: 'Hi, what would you like to create?',
       choices: ['element', 'component', 'container', 'pattern', 'page', 'template']
     }, {
       type: 'input',
@@ -34,48 +31,42 @@ module.exports = class extends Generator {
       type: 'input',
       name: 'elements',
       message: 'What elements will this have? (i.e. \'my-el, my-other-el\')',
-    }]).then((answers) => {
-      this.props = answers;
-    });
+    }]).then((answers) => this.answers = answers);
 
   }
 
-
-
   writting() {
+    this.answers.name_dash = this.answers.name.replace(/ /g, '-').replace(/_/g, '-').toLowerCase();
+    this.answers.name_underscore = this.answers.name.replace(/ /g, '_').replace(/-/g, '_').toLowerCase();
+    this.answers.elements_dash = this.answers.elements.replace(/ /g, '').replace(/_/g, '-').toLowerCase();
+    this.answers.elements_underscore = this.answers.elements.replace(/ /g, '').replace(/-/g, '_').toLowerCase();
 
-    this.props.name_dash = this.props.name.replace(/ /g, "-").replace(/_/g, "-").toLowerCase();
-    this.props.name_underscore = this.props.name.replace(/ /g, "_").replace(/-/g, "_").toLowerCase();
-
-    this.props.elements_dash = this.props.elements.replace(/ /g, "").replace(/_/g, "-").toLowerCase();
-    this.props.elements_underscore = this.props.elements.replace(/ /g, "").replace(/-/g, "_").toLowerCase();
-
-    switch (this.props.type) {
-      case "element":
-        var path = "library/01-elements/" + this.props.name_dash + "/";
+    switch (this.answers.type) {
+      case 'element':
+        var path = `library/01-elements/${this.answers.name_dash}/`;
         break;
-      case "component":
-        var path = "library/02-components/" + this.props.name_dash + "/";
+      case 'component':
+        var path = `library/02-components/${this.answers.name_dash}/`;
         break;
-      case "container":
-        var path = "library/03-containers/" + this.props.name_dash + "/";
+      case 'container':
+        var path = `library/03-containers/${this.answers.name_dash}/`;
         break;
-      case "pattern":
-        var path = "library/04-patterns/" + this.props.name_dash + "/";
+      case 'pattern':
+        var path = `library/04-patterns/${this.answers.name_dash}/`;
         break;
-      case "template":
-        var path = "library/05-templates/" + this.props.name_dash + "/";
+      case 'template':
+        var path = `library/05-templates/${this.answers.name_dash}/`;
         break;
-      case "page":
-        var path = "library/06-pages/" + this.props.name_dash + "/";
+      case 'page':
+        var path = `library/06-pages/${this.answers.name_dash}/`;
         break;
     }
 
-    if (this.props.type == 'template') {
+    if (this.answers.type == 'template') {
       this.fs.copyTpl(
         this.templatePath('_template.hbs'),
-        this.destinationPath(path + this.props.name_dash + '.hbs'), {
-          props: this.props
+        this.destinationPath(path + this.answers.name_dash + '.hbs'), {
+          props: this.answers
         }
       );
 
@@ -83,8 +74,8 @@ module.exports = class extends Generator {
 
       this.fs.copyTpl(
         this.templatePath('_component.hbs'),
-        this.destinationPath(path + this.props.name_dash + '.hbs'), {
-          props: this.props
+        this.destinationPath(path + this.answers.name_dash + '.hbs'), {
+          props: this.answers
         }
       );
 
@@ -92,31 +83,31 @@ module.exports = class extends Generator {
     this.fs.copyTpl(
       this.templatePath('_README.md'),
       this.destinationPath(path + 'README.md'), {
-        props: this.props
+        props: this.answers
       }
     );
     this.fs.copyTpl(
       this.templatePath('_component.tests.js'),
-      this.destinationPath(path + this.props.name_dash + '.tests.js'), {
-        props: this.props
+      this.destinationPath(path + this.answers.name_dash + '.tests.js'), {
+        props: this.answers
       }
     );
-    if (this.props.type == 'component' || this.props.type == 'template' || this.props.type == 'element' || this.props.type == 'container') {
+    if (this.answers.type == 'component' || this.answers.type == 'template' || this.answers.type == 'element' || this.answers.type == 'container') {
 
       this.fs.copyTpl(
         this.templatePath('_component.scss'),
-        this.destinationPath(path + this.props.name_dash + '.scss'), {
-          props: this.props
+        this.destinationPath(path + this.answers.name_dash + '.scss'), {
+          props: this.answers
         }
       );
 
     }
-    if (this.props.type == 'component' || this.props.type == 'container' || this.props.type == 'element' || this.props.type == 'page') {
+    if (this.answers.type == 'component' || this.answers.type == 'container' || this.answers.type == 'element' || this.answers.type == 'page') {
 
       this.fs.copyTpl(
         this.templatePath('_component.config.yml'),
-        this.destinationPath(path + this.props.name_dash + '.config.yml'), {
-          props: this.props
+        this.destinationPath(path + this.answers.name_dash + '.config.yml'), {
+          props: this.answers
         }
       );
 
